eee53217ca65c78969e2621d908e2b1eaada98ff
Titiplex <titouan.johanny@alumni.univ-avignon.fr>
2026-09-17 18:17:07 +0200

    TP1-Ex4: code coverage for swapping to identical values

--- src/test/java/liste/ListeSimpleTest.java
+++ src/test/java/liste/ListeSimpleTest.java
@@ -256,4 +256,18 @@ public class ListeSimpleTest {
         System.out.println(listeATester);
         assertEquals("ListeSimple(Noeud(4), Noeud(2), Noeud(3), Noeud(1), Noeud(5))", listeATester.toString());
     }
+
+    @Test
+    void echangerDeuxNoeudsIdentiques() {
+        listeATester.ajout(5);
+        listeATester.ajout(4);
+        Noeud r1 = listeATester.tete;
+        listeATester.ajout(3);
+        listeATester.ajout(4);
+        Noeud r2 = listeATester.tete;
+        listeATester.ajout(1);
+        assertEquals("ListeSimple(Noeud(1), Noeud(4), Noeud(3), Noeud(4), Noeud(5))", listeATester.toString());
+        listeATester.echanger(r1, r2);
+        assertEquals("ListeSimple(Noeud(1), Noeud(4), Noeud(3), Noeud(4), Noeud(5))", listeATester.toString());
+    }
 }
